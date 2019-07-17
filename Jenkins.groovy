6da89ed7ef8c389996aff157fc8e6c0734ec81ad
Josh Quinn <joshquinn@sidero.ie>
2019-07-17 14:51:07 +0100

    Updated Jenkinsfile

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,7 +1,33 @@
-node{
-    stage(){
-        sh 'mvn clean package'
+node {
+    notify('CI')
+    try {
+        stage('Source Control Checkout') {
+            cehckout scm
+        }
+
+        stage('Test'){
+            sh mvn
+        }
+
+        stage('package') {
+            sh 'mvn clean package'
+        }
+        stage('archive') {
+            archiveArtifacts allowEmptyArchive: true, artifacts: '*.txt'
+            archiveArtifacts allowEmptyArchive: true, artifacts: 'target/*.jar'
+        }
+
+    }catch(err){
+        notify("Error ${err}")
+        currentBuild.result = 'Failure'
     }
+}
 
-
+def notfiy(status){
+    emailtext(
+            to: "jbqjenkins@gmail.com",
+            subject: "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+            body:"""<p>${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
+    )
 }
